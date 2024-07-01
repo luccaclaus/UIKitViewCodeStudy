eaa0fc21fae43659c8e919c1fef544c56bd9bf3b
luccaclaus <luccaclaus@hotmail.com>
2024-07-01 15:28:46 -0300

    added context menus

--- UIKitViewCode/UIKitViewCode/FirstScreen.swift
+++ UIKitViewCode/UIKitViewCode/FirstScreen.swift
@@ -6,25 +6,39 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class FirstScreen: UIViewController {
     
+    let imageView = UIImageView()
     let nextButton = UIButton()
+        
+    let stack = UIStackView() //test stack
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let interaction = UIContextMenuInteraction(delegate: self)
+        nextButton.addInteraction(interaction)
+        drawRectangle()
         setupButton()
         view.backgroundColor = .systemBackground
         title = "First Screen"
         navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+    }
+    
     func setupButton() {
         view.addSubview(nextButton)
         
         nextButton.configuration = .filled()
         nextButton.configuration?.baseBackgroundColor = .systemBrown
-//        nextButton.configuration?.image = UIImage(systemName: "plus")
+        //        nextButton.configuration?.image = UIImage(systemName: "plus")
         
         let imageConfig = UIImage.SymbolConfiguration(pointSize: 30, weight: .bold, scale: .large)
         let boldImg = UIImage(systemName: "plus", withConfiguration: imageConfig)
@@ -41,11 +55,69 @@ class FirstScreen: UIViewController {
         ])
     }
     
+    
+    
     @objc func goToNextScreen() {
         let nextScreen = SecondScreen()
         nextScreen.title = "Second Screen"
         navigationController?.pushViewController(nextScreen, animated: true)
     }
+    
+    
+    func drawRectangle() {
+        view.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
 
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+        }
+        
+        imageView.image = img
+    }
+    
+
+
+}
+
+extension FirstScreen: UIContextMenuInteractionDelegate {
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { suggestedActions in
+            
+            // Create an action for sharing
+            let share = UIAction(title: "Share", image: UIImage(systemName: "square.and.arrow.up")) { action in
+                // Show system share sheet
+            }
+            
+            // Create an action for renaming
+            let rename = UIAction(title: "Rename", image: UIImage(systemName: "square.and.pencil")) { action in
+                // Perform renaming
+            }
+            
+            // Here we specify the "destructive" attribute to show that it’s destructive in nature
+            let delete = UIAction(title: "Delete", image: UIImage(systemName: "trash"), attributes: .destructive) { action in
+                // Perform delete
+            }
+            
+            // Create and return a UIMenu with all of the actions as children
+            return UIMenu(title: "", children: [share, rename, delete])
+        }
+        
+    }
 }
 
